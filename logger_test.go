4dbd6e4089ca0557f0ca5c4dd7cb27f182ecd42f
Michael P. Gerlek <mpg@flaxen.com>
2016-01-30 21:34:12 -0500

    WIP - converting to interfaces

--- logger_test.go
+++ logger_test.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"encoding/json"
-	//assert "github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
 	piazza "github.com/venicegeo/pz-gocommon"
-	"io/ioutil"
-	"net/http"
 	"testing"
 	"time"
 )
@@ -37,59 +34,12 @@ func TestRunSuite(t *testing.T) {
 	suite.Run(t, s)
 }
 
-func checkValidAdminResponse(t *testing.T, resp *http.Response) {
-	defer resp.Body.Close()
+func checkMessageArrays(t *testing.T, actualMssgs []piazza.LogMessage, expectedMssgs []piazza.LogMessage) {
+	assert.Equal(t, len(expectedMssgs), len(actualMssgs), "wrong number of log messages")
 
-	if resp.StatusCode != http.StatusOK {
-		t.Fatalf("bad admin response: %s", resp.Status)
-	}
-
-	data, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		t.Fatalf("%v", err)
-	}
-
-	var m piazza.AdminResponse
-	err = json.Unmarshal(data, &m)
-	if err != nil {
-		t.Fatalf("unmarshall of admin response: %v", err)
-	}
-
-	if time.Since(m.StartTime).Seconds() > 5 {
-		t.Fatalf("service start time too long ago")
-	}
-
-	if m.Logger == nil {
-		t.Fatal("admin response didn't have logger data set")
-	}
-	if m.Logger.NumMessages != 2 {
-		t.Fatalf("wrong number of logs")
-	}
-}
-
-func checkValidResponse(t *testing.T, resp *http.Response) {
-	defer resp.Body.Close()
-
-	data, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		t.Fatalf("%v", err)
-	}
-
-	if resp.StatusCode != http.StatusOK {
-		t.Fatalf("bad post response: %s: %s", resp.Status, string(data))
-	}
-}
-
-func checkValidResponse2(t *testing.T, actualMssgs []piazza.LogMessage, expectedMssgs []piazza.LogMessage) {
-
-	if len(actualMssgs) != len(expectedMssgs) {
-		t.Fatalf("expected %d mssgs, got %d", len(expectedMssgs), len(actualMssgs))
-	}
 	for i := 0; i < len(actualMssgs); i++ {
 		if actualMssgs[i] != expectedMssgs[i] {
-			t.Logf("Expected[%d]: %v\n", i, expectedMssgs[i])
-			t.Logf("Actual[%d]:   %v\n", i, actualMssgs[i])
-			t.Fatalf("returned log incorrect")
+			assert.Equal(t, expectedMssgs[i], actualMssgs[i], "message %d not equal", i)
 		}
 	}
 }
@@ -103,6 +53,10 @@ func (suite *LoggerTester) TestOkay() {
 
 	client := NewPzLoggerClient("localhost:12341")
 
+	assert := assert.New(t)
+
+	////
+
 	data1 := piazza.LogMessage{
 		Service:  "log-tester",
 		Address:  "128.1.2.3",
@@ -111,19 +65,15 @@ func (suite *LoggerTester) TestOkay() {
 		Message:  "The quick brown fox",
 	}
 	err = client.PostToMessages(&data1)
-	if err != nil {
-		t.Fatalf("%s", err)
-	}
+	assert.NoError(err, "PostToMessages")
 
 	actualMssgs, err = client.GetFromMessages()
-	if err != nil {
-		t.Fatalf("%s", err)
-	}
+	assert.NoError(err, "GetFromMessages")
 
 	expectedMssgs = []piazza.LogMessage{data1}
-	checkValidResponse2(t, actualMssgs, expectedMssgs)
+	checkMessageArrays(t, actualMssgs, expectedMssgs)
 
-	///////////////////
+	////
 
 	data2 := piazza.LogMessage{
 		Service:  "log-tester",
@@ -134,52 +84,35 @@ func (suite *LoggerTester) TestOkay() {
 	}
 
 	err = client.PostToMessages(&data2)
-	if err != nil {
-		t.Fatalf("post failed: %s", err)
-	}
+	assert.NoError(err, "PostToMessages")
 
 	actualMssgs, err = client.GetFromMessages()
-	if err != nil {
-		t.Fatalf("get failed: %s", err)
-	}
+	assert.NoError(err, "GetFromMessages")
 
 	expectedMssgs = []piazza.LogMessage{data1, data2}
-	checkValidResponse2(t, actualMssgs, expectedMssgs)
+	checkMessageArrays(t, actualMssgs, expectedMssgs)
 
 	stats, err := client.GetFromAdminStats()
-	if err != nil {
-		t.Fatalf("admin get failed: %s", err)
-	}
-	if stats.NumMessages != 2 {
-		t.Fatalf("stats wrong, expected 3, got %d", stats.NumMessages)
-	}
+	assert.NoError(err, "GetFromAdminStats")
+	assert.Equal(2, stats.NumMessages, "stats check")
+	assert.WithinDuration(time.Now(), stats.StartTime, 5*time.Second, "service start time too long ago")
+
+	////
 
 	err = pzService.Log(piazza.SeverityInfo, "message from pz-logger unit test via piazza.Log()")
-	if err != nil {
-		t.Fatalf("piazza.Log() failed: %s", err)
-	}
+	assert.NoError(err, "pzService.Log()")
 
 	////
 
 	settings, err := client.GetFromAdminSettings()
-	if err != nil {
-		t.Fatalf("admin settings get failed: %s", err)
-	}
-	if settings.Debug {
-		t.Error("settings get had invalid response")
-	}
+	assert.NoError(err, "GetFromAdminSettings")
+	assert.False(settings.Debug, "settings.Debug")
 
 	settings.Debug = true
 	err = client.PostToAdminSettings(settings)
-	if err != nil {
-		t.Fatalf("admin settings post failed: %s", err)
-	}
+	assert.NoError(err, "PostToAdminSettings")
 
 	settings, err = client.GetFromAdminSettings()
-	if err != nil {
-		t.Fatalf("admin settings get failed: %s", err)
-	}
-	if !settings.Debug {
-		t.Error("settings get had invalid response")
-	}
+	assert.NoError(err, "GetFromAdminSettings")
+	assert.True(settings.Debug, "settings.Debug")
 }
