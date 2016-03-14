355e04e87bffe3fc97251d3d3795298c1f052fe5
mpg <mpg@flaxen.com>
2016-03-14 13:18:27 -0400

    added new CustomLogger helper class

--- client/common.go
+++ client/common.go
@@ -36,15 +36,60 @@ type ILoggerService interface {
 	GetAddress() string
 
 	// low-level interfaces
-	PostToMessages(*LogMessage) error
 	GetFromMessages() ([]LogMessage, error)
 	GetFromAdminStats() (*LoggerAdminStats, error)
 	GetFromAdminSettings() (*LoggerAdminSettings, error)
 	PostToAdminSettings(*LoggerAdminSettings) error
 
 	// high-level interfaces
-	Log(service piazza.ServiceName, address string, severity Severity, message string, t time.Time) error
+	LogMessage(mssg *LogMessage) error
+	Log(service piazza.ServiceName, address string, severity Severity, t time.Time, message string, v ...interface{}) error
 }
+
+//---------------------------------------------------------------------------
+
+// CustomLogger is for convenience, allowing the logger user to avoid passing all the other params.
+type CustomLogger struct {
+	iLogger    *ILoggerService
+	myServiceName piazza.ServiceName
+	myAddress     string
+}
+
+func NewCustomLogger(logger *ILoggerService, serviceName piazza.ServiceName, address string) *CustomLogger {
+	return &CustomLogger{iLogger: logger, myServiceName: serviceName, myAddress: address}
+}
+
+func (logger *CustomLogger) post(severity Severity, message string, v ...interface{}) error {
+	str := fmt.Sprintf(message, v...)
+	return (*logger.iLogger).Log(logger.myServiceName, logger.myAddress, severity, time.Now(), str)
+}
+
+// Debug sends a Debug-level message to the logger.
+func (logger *CustomLogger) Debug(message string, v ...interface{}) error {
+	return logger.post(SeverityDebug, message, v...)
+}
+
+// Info sends an Info-level message to the logger.
+func (logger *CustomLogger) Info(message string, v ...interface{}) error {
+	return logger.post(SeverityInfo, message, v...)
+}
+
+// Warn sends a Waring-level message to the logger.
+func (logger *CustomLogger) Warn(message string, v ...interface{}) error {
+	return logger.post(SeverityWarning, message, v...)
+}
+
+// Error sends a Error-level message to the logger.
+func (logger *CustomLogger) Error(message string, v ...interface{}) error {
+	return logger.post(SeverityError, message, v...)
+}
+
+// Fatal sends a Fatal-level message to the logger.
+func (logger *CustomLogger) Fatal(message string, v ...interface{}) error {
+	return logger.post(SeverityFatal, message, v...)
+}
+
+//---------------------------------------------------------------------------
 
 type LoggerAdminStats struct {
 	StartTime   time.Time `json:"starttime"`
