622f1f0ec6be0c59dfb69b43a84a8002a0642dc4
mpg <mpg@flaxen.com>
2016-07-08 15:22:10 -0400

    typo

--- demo/get.sh
+++ demo/get.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-url="https://pz-logger.int.geointservices.io/message?perPage=10"
+url="https://pz-logger.int.geointservices.io/message?perPage=10&sortBy=createdOn&orderBy=desc"
+url="https://pz-logger.int.geointservices.io/message?perPage=6"
 echo
 echo GET $url
 echo "$json"
 
-ret=$(curl -S -s -XPOST -d "$json" "$url")
+ret=$(curl -S -s -XGET -d "$json" "$url")
 
 echo RETURN:
 echo "$ret"
