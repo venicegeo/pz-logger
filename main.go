f23a2ebaa9b573f53696c460a84dad7677667988
mpg <mpg@flaxen.com>
2016-07-12 16:32:02 -0400

    Fine. Be that way. We'll just start an entirely new database index and see how you like _that_.
    Harrumph.

--- main.go
+++ main.go
@@ -31,7 +31,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	idx, err := elasticsearch.NewIndex(sys, "pzlogger", "")
+	idx, err := elasticsearch.NewIndex(sys, "pzlogger2", "")
 	if err != nil {
 		log.Fatal(err)
 	}
