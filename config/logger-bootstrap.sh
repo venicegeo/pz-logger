377c503a7aa6d1bd752c75f0f8d04b222f9b0019
Sonny Saniev <sonny.saniev@rgi-corp.com>
2016-07-13 14:47:29 -0400

    update to vagrant instance

--- config/logger-bootstrap.sh
+++ config/logger-bootstrap.sh
@@ -6,7 +6,7 @@ sudo apt-get upgrade
 #sudo apt-get purge openjdk*
 #sudo apt-get -y install openjdk-7-jdk
 
-#alias list="ls -a"
+alias list="ls -a"
  
 #get golang
 cd /usr/local
@@ -14,49 +14,25 @@ sudo wget https://storage.googleapis.com/golang/go1.6.2.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.6.2.linux-amd64.tar.gz
 
 #add go to path
-#cd /etc/profile.d
-#sudo echo "export PATH=$PATH:/usr/local/go/bin" >> /etc/profile.d/gosetup.sh
 echo 'export PATH=$PATH:/usr/local/go/bin' >>/home/vagrant/.bash_profile
 export PATH=$PATH:/usr/local/go/bin
-#echo $PATH
 
-echo go help.
-go help
 
-#apt-get install git-all
-#apt-get install git htop -y -q
+# install git
 apt-get -y install git
 
-echo PRINT USER
-whoami
-
-#su - vagrant
-
-#echo PRINT USER ..................................
-#whoami
-
-#cd /etc/profile.d
-#touch addgolangtopath.sh
-#echo 'export GOPATH=/home/vagrant/workspace/gostuff' >> addgolangtopath.sh
-
-#apt-get install golang-go
-#apt-get -y install golang-go
-
 #build and start the go app
-#echo Creating new workspace directory at home directory
 mkdir /home/vagrant/workspace
 cd /home/vagrant/workspace
 
-#echo Cloning pz-logger repository...
-#git clone https://github.com/venicegeo/pz-logger.git
-#cd pz-logger
-#echo Creating gogo directory within pz-logger for GOPATH
-#mkdir gogo
-
 #echo setting GOPATH...
-#export GOPATH=$(pwd -P)/workspace/pz-logger/gogo
 export GOPATH=/home/vagrant/workspace/gostuff
 #echo 'export GOPATH=/home/vagrant/workspace/gostuff' >>/home/vagrant/.bash_profile
+
+#ENV variables for logger
+export VCAP_SERVICES='{"user-provided":[{"credentials":{"host":"192.168.44.44:9200","hostname":"192.168.44.44","port":"9200"},"label":"user-provided","name":"pz-elasticsearch","syslog_drain_url":"","tags":[]}]}'
+export PORT=14600
+export VCAP_APPLICATION='{"application_id": "14fca253-8081-402e-abf5-8fd40ddda81f","application_name": "pz-logger","application_uris": ["pz-logger.int.geointservices.io"],"application_version": "5f0ee99-q252c-4f8d-b241-bc3e22534afc","limits": {"disk": 1024,"fds": 16384,"mem": 512},"name": "pz-logger","space_id": "d65a0987-df00-4d69-a50b-657e52cb2f8e","space_name": "simulator-stage","uris": ["pz-logger.int.geointservices.io"],"users": null,"version": "5f0ee99d-252c-4f8d-b241-bc3e22534afc"}'
 
 #echo getting pz-logger and trying to build it...
 go get github.com/venicegeo/pz-logger
