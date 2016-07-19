14c4bd9aba9c8af94b4c4b6f33710386e9807f8a
Sonny Saniev <sonny.saniev@rgi-corp.com>
2016-07-18 20:21:37 -0400

    env and boot up start scripts

--- config/logger-bootstrap.sh
+++ config/logger-bootstrap.sh
@@ -34,18 +34,23 @@ export VCAP_SERVICES='{"user-provided":[{"credentials":{"host":"192.168.44.44:92
 export PORT=14600
 export VCAP_APPLICATION='{"application_id": "14fca253-8081-402e-abf5-8fd40ddda81f","application_name": "pz-logger","application_uris": ["pz-logger.int.geointservices.io"],"application_version": "5f0ee99-q252c-4f8d-b241-bc3e22534afc","limits": {"disk": 1024,"fds": 16384,"mem": 512},"name": "pz-logger","space_id": "d65a0987-df00-4d69-a50b-657e52cb2f8e","space_name": "simulator-stage","uris": ["pz-logger.int.geointservices.io"],"users": null,"version": "5f0ee99d-252c-4f8d-b241-bc3e22534afc"}'
 
-#copying required init scripts
+#copying required set env script to profile.d for startup of the box
 chmod 777 /vagrant/pzlogger/config/logger-env-variables.sh
 cp /vagrant/pzlogger/config/logger-env-variables.sh /etc/profile.d/logger-env-variables.sh
 
-chmod 777 /vagrant/pzlogger/config/logger-startup.sh
-cp /vagrant/pzlogger/config/logger-startup.sh /etc/init.d/logger-startup.sh
+#chmod 777 /vagrant/pzlogger/config/logger-startup.sh
+#cp /vagrant/pzlogger/config/logger-startup.sh /etc/init.d/logger-startup.sh
+
+#adding startup scripts to /etc/rc.local
+cd /etc
+echo '#!/bin/sh -e' > rc.local
+echo 'su - root -c /home/vagrant/workspace/gostuff/bin/pz-logger &' >> rc.local
+echo 'exit 0' >> rc.local
 
 #echo getting pz-logger and trying to build it...
 cd /home/vagrant/workspace
 go get github.com/venicegeo/pz-logger
 go install github.com/venicegeo/pz-logger
 
-#start pz-logger app
-#cd /home/vagrant/workspace/gostuff/bin
-#./pz-logger
+#start the app on initial box setup.
+/home/vagrant/workspace/gostuff/bin/pz-logger
