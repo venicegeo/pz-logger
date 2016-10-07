fb1d6487021798da8982d74a838a099ae492c219
mpg <mpg@flaxen.com>
2016-10-07 11:15:22 -0400

    turn on vendoring

--- ci/test.sh
+++ ci/test.sh
@@ -9,22 +9,16 @@ popd > /dev/null
 export GOPATH=$root/gogo
 mkdir -p "$GOPATH"
 
-# glide expects this to already exist
+# glide expects these to already exist
 mkdir "$GOPATH"/bin "$GOPATH"/src "$GOPATH"/pkg
 
 PATH=$PATH:"$GOPATH"/bin
-
-export GO15VENDOREXPERIMENT="1"
 
 curl https://glide.sh/get | sh
 
 # get ourself, and go there
 go get github.com/venicegeo/pz-logger
 cd $GOPATH/src/github.com/venicegeo/pz-logger
-
-#glide install
-#glide update
-go get github.com/stretchr/testify/assert
 
 #----------------------------------------------------------------------
 
