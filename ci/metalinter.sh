94bf724dcd7571e6a302ef14a1533f84a22fcb4b
mpg <mpg@flaxen.com>
2016-10-10 18:10:44 -0400

    updated to match current norms

--- ci/metalinter.sh
+++ ci/metalinter.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
 gometalinter \
---deadline=45s \
---concurrency=8 \
+--deadline=60s \
+--concurrency=6 \
 --vendor \
+--exclude="exported (var)|(method)|(const)|(type)|(function) [A-Za-z\.0-9]* should have comment" \
+--exclude="comment on exported function [A-Za-z\.0-9]* should be of the form" \
+--exclude="Api.* should be .*API" \
+--exclude="Http.* should be .*HTTP" \
+--exclude="Id.* should be .*ID" \
+--exclude="Json.* should be .*JSON" \
+--exclude="Url.* should be .*URL" \
 ./...
-
-#--exclude="exported method|const|type|function [A-Za-z\.0-9]* should have comment" \
-#--exclude="cyclomatic complexity 13 of function createQueryDslAsString" \
-#--exclude="cyclomatic complexity 12 of function \(\*Service\)\.GetMessage" \
-#--exclude="cyclomatic complexity 11 of function \(\*Client\)\.GetMessages" \
-
 
