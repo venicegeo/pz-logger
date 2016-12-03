8d58081ae1619cccdee036a76248db574e1213fb
mpg <mpg@flaxen.com>
2016-12-03 00:30:47 -0500

    last try

--- logger/Service.go
+++ logger/Service.go
@@ -145,7 +145,11 @@ func (service *Service) getMessages(params *piazza.HttpQueryParams, format strin
 
 	var lines = make([]syslogger.Message, 0)
 
+	log.Printf("got hits?")
+
 	if searchResult != nil && searchResult.GetHits() != nil {
+		log.Printf("got hits: %d", len(*searchResult.GetHits()))
+
 		for _, hit := range *searchResult.GetHits() {
 			if hit.Source == nil {
 				log.Printf("null source hit")
