d7588f6e96f3ad888f11d6d7c29650cafaea1389
mpg <mpg@flaxen.com>
2016-12-03 00:54:48 -0500

    Revert "debugging"
    This reverts commit 19a9598b6e13d7cb150baf66f57a40c6854700ef.

--- logger/ElasticsearchWriter.go
+++ logger/ElasticsearchWriter.go
@@ -26,7 +26,7 @@ import (
 
 //---------------------------------------------------------------------
 
-const schemaMappingX = `{
+const schemaMapping = `{
 	"dynamic": "strict",
 	"properties": {
 		"facility": {
@@ -57,39 +57,6 @@ const schemaMappingX = `{
 			"type": "string"
 		}
 	}
-}`
-
-const schemaMapping = `{
-{
-"dynamic": "strict",
-"properties": {
-		"service": {
-				"type": "string",
-				"store": true,
-				"index": "not_analyzed"
-		},
-		"address": {
-				"type": "string",
-				"store": true,
-				"index": "not_analyzed"
-		},
-		"createdOn": {
-				"type": "date",
-				"store": true,
-				"index": "not_analyzed"
-		},
-		"severity": {
-				"type": "string",
-				"store": true,
-				"index": "not_analyzed"
-		},
-		"message": {
-				"type": "string",
-				"store": true,
-				"index": "analyzed"
-		}
-}
-}
 }`
 
 const logSchema = "LogData7x"
