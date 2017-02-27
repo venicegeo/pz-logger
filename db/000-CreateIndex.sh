8144ca59716d1940aa0a81e4113a5019bf64463e
wkhunt <wkhunt3@gmail.com>
2017-02-27 10:59:33 -0500

    Reduced clutter

--- db/000-CreateIndex.sh
+++ db/000-CreateIndex.sh
@@ -71,7 +71,9 @@ if [[ $TESTING == "" ]]; then
 fi
 
 function removeAliases {
-  echo "Running remove alias function"
+  if [ "$TESTING" = true ] ; then
+    echo "Running remove alias function"
+  fi
   crash=$1
 
   #
@@ -94,7 +96,9 @@ function removeAliases {
   regex=""\""alias"\"":"\""$ALIAS_NAME"\"","\""index"\"":"\""([^"\""]+)"
   temp=`echo $getAliasesCurl|grep -Eo $regex | cut -d\" -f8`
   indexArr=(${temp// / })
-  echo "Found ${#indexArr[@]} indices currently using alias $ALIAS_NAME: ${indexArr[@]}"
+  if [ "$TESTING" = true ] ; then
+    echo "Found ${#indexArr[@]} indices currently using alias $ALIAS_NAME: ${indexArr[@]}"
+  fi
 
   #
   # Remove alias from all above indices
@@ -114,12 +118,16 @@ function removeAliases {
         exit 1
       fi
     fi
-    echo "Removed alias $ALIAS_NAME on index $index"
+	if [ "$TESTING" = true ] ; then
+      echo "Removed alias $ALIAS_NAME on index $index"
+    fi
   done
 }
 
 function createAlias {
-  echo "Running create alias function"
+  if [ "$TESTING" = true ] ; then
+    echo "Running create alias function"
+  fi
   crash=$1
 
   #
@@ -138,14 +146,18 @@ function createAlias {
       exit 1
     fi
   fi
-  echo "Created alias $ALIAS_NAME on index $INDEX_NAME"
+  if [ "$TESTING" = true ] ; then
+    echo "Created alias $ALIAS_NAME on index $INDEX_NAME"
+  fi
 }
 
 #
 # Check to see if index already exists
 #
 
-echo "Checking to see if index $INDEX_NAME already exists..."
+if [ "$TESTING" = true ] ; then
+  echo "Checking to see if index $INDEX_NAME already exists..."
+fi
 cat=_cat
 catCurl=`curl -X GET -H "Content-Type: application/json" -H "Cache-Control: no-cache" "$ES_IP$cat/indices" --write-out %{http_code} 2>/dev/null`
 http_code=`echo $catCurl | cut -d] -f2`
@@ -165,9 +177,10 @@ fi
 # Create the index
 #
 
-echo "Creating index $INDEX_NAME with mappings..."
+if [ "$TESTING" = true ] ; then
+  echo "Creating index $INDEX_NAME with mappings..."
+fi
 createIndexCurl=`curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache" -d "$IndexSettings" "$ES_IP$INDEX_NAME" --write-out %{http_code} 2>/dev/null`
-echo $createIndexCurl
 http_code=`echo $catCurl | cut -d] -f2`
 if [[ $createIndexCurl != '{"acknowledged":true}200' ]]; then
   echo "Failed to create index $INDEX_NAME. Code: $http_code"
@@ -196,5 +209,4 @@ removeAliases false
 
 createAlias true
 
-echo 
 echo "Success!"
