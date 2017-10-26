5ea114bbd6c6972479bed0375f4c9c75986d04d8
johnmcmahon <John.McMahon@digitalglobe.com>
2017-10-25 21:48:12 -0400

    Fix GetURL call

--- main.go
+++ main.go
@@ -89,7 +89,7 @@ func setupES(sys *piazza.SystemConfig) (elasticsearch.IIndex, pzsyslog.Writer, p
 		if err != nil {
 			return nil, nil, nil, err
 		}
-		esURL, err := sys.GetURLNoProtocol(piazza.PzElasticSearch)
+		esURL, err := sys.GetURL(piazza.PzElasticSearch)
 		if err != nil {
 			return nil, nil, nil, err
 		}
